b6e67b7b07e8f6a65c32742f3428e0b091083675
bernardcryptohk <bernard.cryptohk@gmail.com>
2025-06-05 22:34:55 +0200

    feat: integrate Clerk for authentication and add layout components
    - Added ClerkProvider to layout for user authentication.
    - Created Header component to display user information and authentication buttons.
    - Implemented Sidebar component with a New Document button.
    - Updated Home page to include a simple heading and button.
    - Introduced utility functions for class name merging.
    - Added middleware for Clerk authentication.
    - Updated package dependencies for new components and utilities.
    - Removed unused font imports and adjusted layout styles.

---
+++ components/Header.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+  UserButton,
+  useUser,
+} from "@clerk/nextjs";
+import { Sign } from "crypto";
+
+export default function Header() {
+  // Access user info from Clerk
+  const { user } = useUser();
+  return (
+    <div className="flex items-center justify-between p-5">
+      {user && (
+        <h1 className="text-2xl">
+          {user?.firstName}
+          {`'s`} Space
+        </h1>
+      )}
+
+      {/* Breadcrumbs, directory we are currently in */}
+
+      <div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </div>
+  );
+}
